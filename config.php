216bc60214696451e4782d59b081a433b90a96f4
rawsand <raw.sand85@gmail.com>
2026-02-16 19:05:12 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,11 +1,12 @@
 <?php
-/* Read secrets from Render environment variables */
-define("BOT_TOKEN", getenv("BOT_TOKEN"));
-define("OWNER_ID", getenv("OWNER_ID"));
-define("GITHUB_TOKEN", getenv("GITHUB_TOKEN"));
-define("GITHUB_USERNAME", getenv("GITHUB_USERNAME"));
-define("GITHUB_REPO", getenv("GITHUB_REPO"));
-define("GITHUB_FILE_PATH", "links.txt");
 
-/* Max video size 2GB */
-define("MAX_FILE_SIZE", 2 * 1024 * 1024 * 1024);
+$botToken = getenv('BOT_TOKEN');
+$allowedUser = getenv('OWNER_ID');
+
+$apiURL = "https://api.telegram.org/bot$botToken/";
+
+$githubToken = getenv('GITHUB_TOKEN');
+$githubUsername = getenv('GITHUB_USERNAME');
+$githubRepo = getenv('GITHUB_REPO');
+
+?>
