ce609eb627be3f57872100de2fbc9f6d9bca9d05
rawsand <raw.sand85@gmail.com>
2026-03-06 17:26:21 +0530

    Update pcloud_handler.py

--- pcloud_handler.py
+++ pcloud_handler.py
@@ -1,65 +1,37 @@
 import requests
 
-
 class PcloudHandler:
-
     def __init__(self, token):
         self.token = token
 
     def get_space(self):
-
-        r = requests.get(
-            "https://api.pcloud.com/userinfo",
-            params={"auth": self.token}
-        ).json()
-
-        quota = r["quota"]
-        used = r["usedquota"]
-
-        return quota - used
+        r = requests.get("https://api.pcloud.com/userinfo", params={"auth": self.token}).json()
+        return r["quota"] - r["usedquota"]
 
     def list_files(self):
-
-        r = requests.get(
-            "https://api.pcloud.com/listfolder",
-            params={
-                "auth": self.token,
-                "folderid": 0
-            }
-        ).json()
-
+        r = requests.get("https://api.pcloud.com/listfolder", params={"auth": self.token, "folderid": 0}).json()
         files = []
-
         if "metadata" in r:
             for f in r["metadata"]["contents"]:
                 if not f["isfolder"]:
                     files.append((f["name"], f["fileid"]))
-
         return files
 
     def delete_file(self, fileid):
+        requests.get("https://api.pcloud.com/deletefile", params={"auth": self.token, "fileid": fileid})
 
-        requests.get(
-            "https://api.pcloud.com/deletefile",
-            params={
-                "auth": self.token,
-                "fileid": fileid
-            }
-        )
-
-    def upload_stream(self, stream, filename):
-
+    def upload_stream(self, response_stream, filename, progress_callback=None):
         url = "https://api.pcloud.com/uploadfile"
+        with requests.post(url, params={"auth": self.token, "folderid": 0}, files={"file": (filename, StreamWrapper(response_stream, progress_callback))}) as r:
+            return r.json()
 
-        files = {
-            "file": (filename, stream)
-        }
+class StreamWrapper:
+    def __init__(self, response, progress_callback=None):
+        self.response = response
+        self.progress_callback = progress_callback
 
-        data = {
-            "auth": self.token,
-            "folderid": 0
-        }
-
-        r = requests.post(url, files=files, data=data)
-
-        return r.json()
+    def read(self, chunk_size=1024*1024):
+        chunk = self.response.raw.read(chunk_size)
+        if chunk and self.progress_callback:
+            self.progress_callback(chunk)
+        return chunk
